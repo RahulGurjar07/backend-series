26373dcb152daec565eb35145baf696136e09a7f
RahulGurjar07 <rahulgurjarmp09@gmail.com>
2024-01-02 00:25:45 +0530

    database connection setup

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path: './env'})
+
+// ager dotenv ko import statement se  karna hai toh ye code use karo
+// import dotenv from "dotenv";
+// dotenv.config({path: './env'})
+// or package.json me experimental feature use kar sakte ho
+
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+
+connectDB();
+
+
+
+
+
+
+// this code is for connecting with database and server 
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error) => {
+            console.log("error: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`server is running on port ${process.env.PORT}`)
+        })
+    }
+    catch (error) {
+        console.log("error: ",error)
+        throw error
+    }
+})()
+*/
